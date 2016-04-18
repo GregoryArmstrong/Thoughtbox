c69718d47a703a4a0715976e451d24e1e8d19011
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-04-18 10:53:04 -0600

    Add sad path testing for email address / mismatched passwords
    Added sad path tests to the existing guest_can_register_as_user_spec.rb integration tests
    to check for already used email address or mismatched password fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   has_many :roles, through: :user_roles
 
   validates :username, presence: true
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  # validates :email_address, presence: true, length: { maximum: 255 },
-  #                   format: { with: VALID_EMAIL_REGEX },
-  #                   uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email_address, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :password_digest, presence: true, length: { minimum: 6 }, allow_nil: true
 
   def registered_user?
--- spec/requests/guest_can_register_as_user_spec.rb
+++ spec/requests/guest_can_register_as_user_spec.rb
@@ -33,4 +33,44 @@ RSpec.feature "GuestCanRegisterAsUser", type: :feature do
     expect(page).to have_content("Links Index")
   end
 
+  scenario "Guest cannot sign up with an email address already used" do
+    user_1 = User.create(username: 'GregArm', email_address: "intergalacticgentleman@gmail.com", password: "pass")
+
+    visit root_path
+
+    expect(page).to have_content('Log In or Sign Up')
+
+    click_on('Sign Up')
+
+    expect(current_path).to eq new_user_path
+
+    fill_in('Username', :with => 'GregArm_two')
+    fill_in('Email', :with => 'intergalacticgentleman@gmail.com')
+    fill_in('Password', :with => 'pass')
+    fill_in('Password Confirmation', :with => 'pass')
+    click_on('Submit')
+
+    expect(current_path).to eq root_path
+    expect(User.count).to eq 1
+  end
+
+  scenario "Guest must use matching password and password_confirmation" do
+    visit root_path
+
+    expect(page).to have_content('Log In or Sign Up')
+
+    click_on('Sign Up')
+
+    expect(current_path).to eq new_user_path
+
+    fill_in('Username', :with => 'GregArm_two')
+    fill_in('Email', :with => 'intergalacticgentleman@gmail.com')
+    fill_in('Password', :with => 'pass')
+    fill_in('Password Confirmation', :with => 'not_pass')
+    click_on('Submit')
+
+    expect(current_path).to eq root_path
+    expect(User.count).to eq 0
+  end
+
 end
